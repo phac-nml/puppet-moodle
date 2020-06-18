206a480c0945271a0ab1501a5559e0400567454b
Matt Kraai <mkraai@crosslinktax.com>
2020-06-18 11:48:34 -0700

    Add post-install command(s) for plugins

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -9,6 +9,8 @@ define moodle::plugin (
   Enum['moodle','branch','tag'] $version_type = 'moodle',
   $www_owner                                  = $moodle::www_owner,
   $www_group                                  = $moodle::www_group,
+  Optional[String] $install_cmd               = undef,
+  Optional[Hash] $install_exec                = undef,
 ) {
 
   $plugin_install_dir = $install_subdir ? {
@@ -25,7 +27,6 @@ define moodle::plugin (
       case $version_type {
         'moodle':         {
           $stripped_version = $plugin_version.split('\.')[0,2].join()
-          $git_branch = "MOODLE_${stripped_version}_STABLE"
           $git_branch = $plugin_version ? {
             /^\d+\.\d+$/  => "MOODLE_${stripped_version}_STABLE",
             /^\d\d+$/     => "MOODLE_${stripped_version}_STABLE",
@@ -66,6 +67,32 @@ define moodle::plugin (
       target  => "git-exclude-${moodle_install_dir}",
       content => "/${install_subdir}/",
       order   => '10',
+    }
+  }
+
+  if $install_cmd {
+    exec { "moodle-plugin-${name}-install":
+      command     => $install_cmd,
+      cwd         => $plugin_install_dir,
+      refreshonly => true,
+    }
+    if defined(Git::Repo["moodle-${moodle_install_dir}-${name}"]) {
+      Git::Repo["moodle-${moodle_install_dir}-${name}"] ~> Exec["moodle-plugin-${name}-install"]
+    }
+    if defined(Vcsrepo["moodle-${moodle_install_dir}-${name}"]) {
+      Vcsrepo["moodle-${moodle_install_dir}-${name}"] ~> Exec["moodle-plugin-${name}-install"]
+    }
+  }
+  if $install_exec {
+    exec { "moodle-plugin-${name}-exec":
+      # Default to working directory = install directory; force to refresh only.
+      * => {cwd => $plugin_install_dir,} + $install_exec + {refreshonly => true,},
+    }
+    if defined(Git::Repo["moodle-${moodle_install_dir}-${name}"]) {
+      Git::Repo["moodle-${moodle_install_dir}-${name}"] ~> Exec["moodle-plugin-${name}-exec"]
+    }
+    if defined(Vcsrepo["moodle-${moodle_install_dir}-${name}"]) {
+      Vcsrepo["moodle-${moodle_install_dir}-${name}"] ~> Exec["moodle-plugin-${name}-exec"]
     }
   }
 
